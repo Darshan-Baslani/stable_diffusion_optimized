1f672dfb08c5fc3e17f696290102125e96e37b24
Darshan-Baslani <darshan.baslani4@gmail.com>
2025-10-18 09:59:16 +0530

    add: step function in DDPMSampler

--- naive/sd/ddpm.py
+++ naive/sd/ddpm.py
@@ -21,12 +21,58 @@ class DDMPMSampler:
             (np.arange(0, num_inference_steps) * step_ratio).round()[::-1].copy().astype(np.int64)
         )
 
-    def _get_previous_timesteps(self, timestep: int) -> int:
+    def _get_previous_timestep(self, timestep: int) -> int:
         prev_t = timestep - self.num_training_steps // self.num_inference_steps
         return prev_t
 
-    def _get_vairance(self, timestep: int) -> torch.Tensor:
-        pass
+    def _get_variance(self, timestep: int) -> torch.Tensor:
+        prev_t = self._get_previous_timestep(timestep)
+
+        alpha_t_bar = self.alpha_t_bar[timestep]
+        alpha_t_bar_prev = self.alpha_t_bar[prev_t] if prev_t >= 0 else self.one
+
+        # eq 7 of ddpm paper says we require current_beta_t for calculating var
+        # one way of calculating current_beta_t is by following method which
+        # is maybe not mentioned in the paper, but it is valid
+        current_beta_t = 1 - alpha_t_bar / alpha_t_bar_prev
+
+        # eq 7
+        variance = (1 - alpha_t_bar_prev) / (1 - alpha_t_bar) * current_beta_t
+
+        return variance 
+
+    def step(self, timestep: int, latents: torch.Tensor, model_output: torch.Tensor):
+        prev_timestep = self._get_previous_timestep(timestep)
+
+        # compute alphas and betas
+        alpha_t_bar = self.alpha_t_bar[timestep]
+        alpha_t_bar_prev = self.alpha_t_bar[prev_timestep] if timestep >= 0 else self.one
+        beta_t_bar = 1 = alpha_t_bar
+        beta_t_bar_prev = 1 - alpha_t_bar_prev
+        current_alpha_t = alpha_t_bar / alpha_t_bar_prev
+        current_beta_t = 1 - current_alpha_t
+
+        # eq 15
+        pred_original_sample = ((latents - (beta_t_bar ** 0.5) * model_output) / alpha_t_bar ** 0.5)
+
+        # eq 7 for mean and variance
+        first_term = ((alpha_t_bar_prev ** 0.5 * current_beta_t) / beta_t_bar) * pred_original_sample
+        second_term = ((current_alpha_t ** 0.5) * beta_t_bar_prev / beta_t_bar) * latents
+
+        mean = first_term + second_term
+
+        variance = 0
+        if timestep > 0:
+            noise = torch.randn(model_output.shape, generator=self.generator, 
+                                 device=model_output.device, dtype=model_output.dtype)
+
+            stdev_noise = (self._get_variance(timestep) ** 0.5) * noise
+
+        # sample from distribution
+        pred_prev_sample = mean + stdev_noise
+
+        return pred_prev_sample
+
 
     def add_noise(self, original_samples: torch.FloatTensor, timesteps: torch.FloatTensor) -> torch.FloatTensor:
         # eq 4 ddpm paper

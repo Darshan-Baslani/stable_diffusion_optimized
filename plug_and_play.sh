7adedf42ff94f8e973a04d8eee8c8ca486ae650e
Darshan-Baslani <darshan.baslani4@gmail.com>
2025-10-25 12:00:30 +0530

    fix: plug_and_play.sh

--- plug_and_play.sh
+++ plug_and_play.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 pip install -q lightning
 wget -c https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt -O v1-5-pruned-emaonly.ckpt
-git clone https://github.com/Darshan-Baslani/stable_diffusion_optimized.git
-mv /content/v1-5-pruned-emaonly.ckpt /content/sd_v1_only_code/naive/data/
-cd /content/stable_diffusion_optimized
+mv v1-5-pruned-emaonly.ckpt /content/stable_diffusion_optimized/naive/data/
 python naive/sd/inference.py

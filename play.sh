49e9bdb5ddf8246a66d707647a24886dfc019074
Darshan-Baslani <darshan.baslani4@gmail.com>
2025-10-26 11:45:59 +0530

    add: .sh files sends arguments to inference.py

--- play.sh
+++ play.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /content/stable_diffusion_optimized
-python naive/sd/inference.py
+python naive/sd/inference.py "$@"
--- plug_and_play.sh
+++ plug_and_play.sh
@@ -2,4 +2,4 @@
 pip install -q lightning
 wget -c https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt -O v1-5-pruned-emaonly.ckpt
 mv v1-5-pruned-emaonly.ckpt /content/stable_diffusion_optimized/naive/data/
-python naive/sd/inference.py
+python naive/sd/inference.py "$@"
